ab1b7ee7382fbdbe5f1b587c6029c89e7de30811
ziyadmostafa <ziyad.mostafa@msa.edu.eg>
2026-05-10 04:25:47 +0300

    describe what you changed

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { createServerClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { cookies } from 'next/headers'
 
 export async function middleware(req: NextRequest) {
   // Get the pathname of the request
